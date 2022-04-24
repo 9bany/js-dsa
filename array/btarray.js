db7f1722daa66b9a371c6222e4cd32c63955cb9b
@bany <53461381+9bany@users.noreply.github.com>
2022-04-24 07:50:35 +0000

    move array file to folders

---
+++ array/btarray.js
@@ -0,0 +1,16 @@
+class BTArray {
+  constructor() {
+    this.length = 0
+    this.data = null
+  }
+
+  get = (index) => {
+    return this.data[index]
+  }
+
+  push = (value) => {
+  
+  }
+}
+
+module.exports = BTArray;
--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-class BTArray {
-  constructor() {
-    this.length = 0
-    this.data = null
-  }
-
-  get = (index) => {
-    return this.data[index]
-  }
-
-  push = (value) => {
-  
-  }
-}
-
+const BTArray = require('./array/btarray.js')
 const newArray = new BTArray()
 console.log(newArray)
