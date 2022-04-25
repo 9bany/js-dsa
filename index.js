7214c5b6429c2c15018a2a845c147761dec9c1fc
@bany <53461381+9bany@users.noreply.github.com>
2022-04-25 03:02:16 +0000

    js-das:hashtable:hash-implement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const mergeSort = require("./array/mergesort")
+const HashTable = require("./hashtable/hashtable.js")
 
-let result = mergeSort([0,3,4], [4,6,30, 46])
+let hashTable = new HashTable(50)
 
-console.log(result)
+console.log(hashTable._hash('bany'))
